497d60a01d1aa1a7a6557d98b098d8b67d791623
Spencer Merrow <smerrow@gmu.edu>
2023-03-05 19:50:17 -0500

    added all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: '<h1>My App</h1>'
 })
-export class AppComponent {
-  title = 'my-app';
-}
+export class AppComponent {}
