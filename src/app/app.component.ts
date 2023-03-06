ef5876a987590b355ea87835c4687a274416655e
Spencer Merrow <smerrow@disroot.org>
2023-03-05 21:02:07 -0500

    added additional content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: '<h1>My App</h1>'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title: String = 'my-app';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        RouterModule
     ],
     declarations: [
         AppComponent
